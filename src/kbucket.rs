5ab6ef13b3fb251c9f05a027cf398211176e6aba
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2021-08-23 10:39:31 -0600

    Modify distance filtering to use filter_map

--- src/kbucket.rs
+++ src/kbucket.rs
@@ -563,22 +563,29 @@ where
         log2_distances: &[u64],
         max_nodes: usize,
     ) -> Vec<EntryRefView<'_, TNodeId, TVal>> {
+        // Filter log2 distances to only include those in the closed interval [1, 256]
         let distances = log2_distances
             .iter()
-            .filter(|&d| *d > 0 && *d <= (NUM_BUCKETS as u64))
+            .filter_map(|&d| {
+                if d > 0 && d <= (NUM_BUCKETS as u64) {
+                    Some(d)
+                } else {
+                    None
+                }
+            })
             .collect::<Vec<_>>();
 
-        // apply bending nodes
+        // Apply pending nodes
         for distance in &distances {
-            // the log2 distance ranges from 1-256 and is always 1 more than the bucket index. For this
+            // The log2 distance ranges from 1-256 and is always 1 more than the bucket index. For this
             // reason we subtract 1 from log2 distance to get the correct bucket index.
-            let bucket = &mut self.buckets[(*distance - 1) as usize];
+            let bucket = &mut self.buckets[(distance - 1) as usize];
             if let Some(applied) = bucket.apply_pending() {
                 self.applied_pending.push_back(applied)
             }
         }
 
-        // find the matching nodes
+        // Find the matching nodes
         let mut matching_nodes = Vec::new();
 
         // Note we search via distance in order
