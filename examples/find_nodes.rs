9ba2fc72155da4d72c51bb849ff9d00cfbf2f876
Diva M <divma@protonmail.com>
2022-04-23 10:16:16 +0200

    improve find_nodes.rs

--- examples/find_nodes.rs
+++ examples/find_nodes.rs
@@ -36,15 +36,46 @@
 //!
 //!  For a simple CLI discovery service see [discv5-cli](https://github.com/AgeManning/discv5-cli)
 
+use clap::Parser;
 use discv5::{
     enr,
     enr::{k256, CombinedKey},
     Discv5, Discv5ConfigBuilder,
 };
 use std::{
-    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
+    net::{IpAddr, Ipv4Addr, SocketAddr},
     time::Duration,
 };
+use tracing::{info, warn};
+
+#[derive(Parser)]
+struct FindNodesArgs {
+    /// Ip to bind. To get local Ipv6 - Ipv4 communication use UNSPECIFIED addresses instead of
+    /// LOCALHOST (:: instead of ::1, 0.0.0.0 instead of 127.0.0.1)
+    ip: IpAddr,
+    /// Port to bind
+    port: u16,
+    /// Generate a new key instead of the default testing one.
+    #[clap(long)]
+    generate_key: bool,
+    /// Set the ip and port in the ENR for advertisement to other peers.
+    #[clap(long)]
+    set_enr_socket: bool,
+    /// A remote peer to try to connect to.
+    peer: Option<discv5::Enr>,
+}
+
+impl Default for FindNodesArgs {
+    fn default() -> Self {
+        FindNodesArgs {
+            ip: IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+            set_enr_socket: false,
+            port: 9000,
+            generate_key: false,
+            peer: None,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -55,110 +86,75 @@ async fn main() {
         .with_env_filter(filter_layer)
         .try_init();
 
-    // if there is an address specified use it
-    let address = std::env::args()
-        .nth(1)
-        .map(|addr| addr.parse::<IpAddr>().unwrap());
+    let args = FindNodesArgs::parse();
 
-    let is_ip6 = address.as_ref().map(|ip| ip.is_ipv6());
-    let port = {
-        if let Some(udp_port) = std::env::args().nth(2) {
-            udp_port.parse().unwrap()
-        } else {
-            9000
-        }
+    let enr_key = if args.generate_key {
+        // use a new key if specified
+        CombinedKey::generate_secp256k1()
+    } else {
+        // A fixed key for testing
+        let raw_key =
+            hex::decode("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+                .unwrap();
+        let secret_key = k256::ecdsa::SigningKey::from_bytes(&raw_key).unwrap();
+        CombinedKey::from(secret_key)
     };
 
-    // A fixed key for testing
-    let raw_key =
-        hex::decode("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291").unwrap();
-    let secret_key = k256::ecdsa::SigningKey::from_bytes(&raw_key).unwrap();
-    let mut enr_key = CombinedKey::from(secret_key);
-
-    // use a random key if specified
-    if let Some(generate_key) = std::env::args().nth(3) {
-        if generate_key.parse::<bool>().unwrap() {
-            enr_key = CombinedKey::generate_secp256k1();
-        }
-    }
-
-    // construct a local ENR
     let enr = {
         let mut builder = enr::EnrBuilder::new("v4");
-        // if an IP was specified, use it
-        if let Some(external_address) = address {
-            builder.ip(external_address);
-            if external_address.is_ipv6() {
-                builder.udp6(port);
-            } else {
-                builder.udp4(port);
+        if args.set_enr_socket {
+            match args.ip {
+                IpAddr::V4(ip4) => {
+                    builder.ip4(ip4).udp4(args.port);
+                }
+                IpAddr::V6(ip6) => {
+                    builder.ip6(ip6).udp6(args.port);
+                }
             }
         }
         builder.build(&enr_key).unwrap()
     };
 
-    // if the ENR is useful print it
-    println!("Node Id: {}", enr.node_id());
-    println!("Base64 ENR: {}", enr.to_base64());
-    if enr.udp4_socket().is_some() {
-        println!(
-            "IP: {}, UDP_PORT:{}",
-            enr.ip4().unwrap(),
-            enr.udp4().unwrap()
-        );
-    }
-    if enr.udp6_socket().is_some() {
-        println!(
-            "IP: {}, UDP_PORT:{}",
-            enr.ip6().unwrap(),
-            enr.udp6().unwrap()
-        );
-    }
-
     // default configuration with packet filtering
     // let config = Discv5ConfigBuilder::new().enable_packet_filter().build();
     // default configuration without packet filtering
-    let mut builder = &mut Discv5ConfigBuilder::new();
-    if let Some(is_ip6) = is_ip6 {
-        if is_ip6 {
+    let config = {
+        let mut builder = &mut Discv5ConfigBuilder::new();
+        if args.ip.is_ipv6() {
             println!("Setting dual stack ipv6 mode with mapped addresses enabled");
             builder = builder.ip_mode(discv5::IpMode::Ip6 {
                 enable_mapped_addresses: true,
             });
         }
-    }
-    let config = builder.build();
-
-    // the address to listen on
-    let listen_addr: IpAddr = if is_ip6 == Some(true) {
-        Ipv6Addr::LOCALHOST.into()
-    } else {
-        Ipv4Addr::LOCALHOST.into()
+        builder.build()
     };
-    let socket_addr = SocketAddr::new(listen_addr, port);
+
+    info!("Node Id: {}", enr.node_id());
+    if args.set_enr_socket {
+        // if the ENR is useful print it
+        info!("Base64 ENR: {}", enr.to_base64());
+        if args.ip.is_ipv6() {
+            info!("IpV6 socket: {}", enr.udp6_socket().unwrap());
+        } else {
+            info!("IpV4 socket: {}", enr.udp4_socket().unwrap());
+        }
+    }
+
+    // the address to listen on.
+    let socket_addr = SocketAddr::new(args.ip, args.port);
 
     // construct the discv5 server
     let mut discv5 = Discv5::new(enr, enr_key, config).unwrap();
 
     // if we know of another peer's ENR, add it known peers
-    if let Some(base64_enr) = std::env::args().nth(4) {
-        match base64_enr.parse::<enr::Enr<enr::CombinedKey>>() {
-            Ok(enr) => {
-                println!(
-                    "Remote ENR Read. ip4: {:?}, ip6:{:?}, udp_port {:?}, tcp_port: {:?}",
-                    enr.ip4(),
-                    enr.ip6(),
-                    enr.udp4(),
-                    enr.tcp4()
-                );
-                println!("UDP4 socket {:?}", enr.udp4_socket());
-                println!("UDP6 socket {:?}", enr.udp6_socket());
-                if let Err(e) = discv5.add_enr(enr) {
-                    println!("ENR was not added: {}", e);
-                }
-            }
-            Err(e) => panic!("Decoding ENR failed: {}", e),
-        }
+    if let Some(enr) = args.peer {
+        info!(
+            "Remote ENR read. ip4: {:?}, ip6:{:?}, udp_port {:?}, tcp_port: {:?}",
+            enr.ip4(),
+            enr.ip6(),
+            enr.udp4(),
+            enr.tcp4()
+        );
     }
 
     // start the discv5 service
@@ -175,17 +171,17 @@ async fn main() {
                 // get metrics
                 let metrics = discv5.metrics();
                 let connected_peers = discv5.connected_peers();
-                println!("Connected peers: {}, Active sessions: {}, Unsolicited requests/s: {:.2}", connected_peers, metrics.active_sessions, metrics.unsolicited_requests_per_second);
-                println!("Searching for peers...");
+                info!("Connected peers: {}, Active sessions: {}, Unsolicited requests/s: {:.2}", connected_peers, metrics.active_sessions, metrics.unsolicited_requests_per_second);
+                info!("Searching for peers...");
                 // execute a FINDNODE query
                 match discv5.find_node(target_random_node_id).await {
-                    Err(e) => println!("Find Node result failed: {:?}", e),
+                    Err(e) => warn!("Find Node result failed: {:?}", e),
                     Ok(v) => {
                         // found a list of ENR's print their NodeIds
                         let node_ids = v.iter().map(|enr| enr.node_id()).collect::<Vec<_>>();
-                        println!("Nodes found: {}", node_ids.len());
+                        info!("Nodes found: {}", node_ids.len());
                         for node_id in node_ids {
-                            println!("Node: {}", node_id);
+                            info!("Node: {}", node_id);
                         }
                     }
                 }
