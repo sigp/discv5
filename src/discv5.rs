d5c85c50437b427dd52666cd5e5aeba4215eac87
Diva M <divma@protonmail.com>
2022-05-04 15:57:04 -0500

    fmt

--- src/discv5.rs
+++ src/discv5.rs
@@ -34,7 +34,7 @@ use tokio::sync::{mpsc, oneshot};
 use tracing::{debug, warn};
 
 #[cfg(feature = "libp2p")]
-use {libp2p_core::Multiaddr};
+use libp2p_core::Multiaddr;
 
 // Create lazy static variable for the global permit/ban list
 use crate::metrics::{Metrics, METRICS};
