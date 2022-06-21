cc6f5a73d964c9ee52cd79bda7731f24c7c06926
Emilia Hane <emiliaha95@gmail.com>
2022-06-21 10:33:45 +0200

    Add debug output

--- src/service.rs
+++ src/service.rs
@@ -760,6 +760,7 @@ impl Service {
                                 }
                             })
                             .collect();
+                            debug!("Found new reg peers. Peers: {:?}", new_reg_peers);
                     } else {
                         debug!("No peers found to send regtopics to.");
                     }
