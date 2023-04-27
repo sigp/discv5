041c01f42d27aea5f5336cb68aa7e5ca6cc81000
Age Manning <Age@AgeManning.com>
2023-04-27 12:14:15 +1000

    Update to ENR v0.8.1

--- src/handler/crypto/ecdh.rs
+++ src/handler/crypto/ecdh.rs
@@ -7,10 +7,10 @@ use super::k256::{
 
 pub fn ecdh(public_key: &VerifyingKey, secret_key: &SigningKey) -> Vec<u8> {
     k256::PublicKey::from_affine(
-        (k256::PublicKey::from_sec1_bytes(public_key.to_bytes().as_ref())
+        (k256::PublicKey::from_sec1_bytes(public_key.to_sec1_bytes().as_ref())
             .unwrap()
             .to_projective()
-            * k256::SecretKey::from_be_bytes(&secret_key.to_bytes())
+            * k256::SecretKey::from_slice(&secret_key.to_bytes())
                 .unwrap()
                 .to_nonzero_scalar()
                 .as_ref())
