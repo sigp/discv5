5f34c855a049ac46d0bbd2dfbeaffb0a5a7e275f
jmcph4 <jmcph4.github@gmail.com>
2023-10-09 12:50:32 +1000

    Add doc comments for error/warning strings

--- src/metrics.rs
+++ src/metrics.rs
@@ -9,33 +9,89 @@ lazy_static! {
     pub static ref METRICS: InternalMetrics = InternalMetrics::default();
 }
 
+/* WARNINGS */
+
+/// An attempt to send an empty response to a TALK message failed
 pub const SEND_EMPTY_TALK_RESP_FAIL: &str = "send_empty_talk_response_failed";
+
+/// An attempt to send a WHOAREYOU message failed
 pub const SEND_WHOAREYOU_FAIL: &str = "send_whoareyou_fail";
+
+/// An inbound RPC request timed out
 pub const RPC_REQ_TIMEOUT: &str = "rpc_req_timeout";
+
+/// An inbound RPC request errored
 pub const RPC_REQ_FAIL: &str = "rpc_req_fail";
+
+/// The results of a RPC query did not contain an ENR
 pub const QUERY_RES_ENR_MISSING: &str = "query_res_enr_missing";
+
+/// The callback used for servicing a RPC query was dropped
 pub const QUERY_CALLBACK_DROPPED: &str = "query_callback_dropped";
+
+/// No peers were close enough to satisfy the request
 pub const NO_KNOWN_CLOSEST_PEERS: &str = "no_known_closest_peers";
+
+/// The callback servicing a query failed for an unknown reason
 pub const CALLBACK_FAILED: &str = "callback_failed";
+
+/// A `NodesResponse` was received which contained too many nodes; the remainders will be ignored
 pub const TRUNCATING_NODES: &str = "truncating_nodes";
+
+/// Received the incorrect message type as a response to a request
 pub const INCORRECT_RESP_TYPE: &str = "incorrect_resp_type";
+
+/// Failed to send a response to a request
 pub const SEND_RESP_FAIL: &str = "send_resp_fail";
+
+/// A peer is advertising multiple ENRs
 pub const PEER_MULTIPLE_ENRS: &str = "peer_multiple_enrs";
+
+/// The ID of a response message didn't match the expected ID for the request message
 pub const RPC_RESP_MISMATCH: &str = "rpc_resp_mismatch";
+
+/// Failed to write to a required socket
 pub const SOCK_UPDATE_FAIL: &str = "sock_update_fail";
+
+/// Failed to respond to a FINDNODES message
 pub const SEND_FINDNODES_RESP_FAIL: &str = "send_findnodes_resp_fail";
+
+/// Responding to a NODES message failed, but was able to be partially processed (hence this is a warning)
 pub const RPC_NODE_RESP_FAIL: &str = "rpc_node_resp_fail";
+
+/// Failed to transition a node to the disconnected state
 pub const NODE_UPDATE_DISCONNECT_FAIL: &str = "node_update_disconnect_fail";
+
+/// A peer forwarded us an invalid ENR
 pub const PEER_SENT_BAD_ENR: &str = "peer_sent_bad_enr";
+
+/// An attempt to send an empty response to a FINDNODES message failed
 pub const SEND_EMPTY_FINDNODES_RESP_FAIL: &str = "send_empty_findnodes_resp_fail";
 
+/* ERRORS */
+
+/// No request handler was found for this request
 pub const NO_MATCHING_REQ_CALL: &str = "no_matching_req_call";
+
+/// No matching nonce was found
 pub const NO_MATCHING_NONCE: &str = "no_matching_nonce";
+
+/// Failed to generate a session
 pub const SESS_GENERATE_FAIL: &str = "sess_generate_fail";
+
+/// Insertion into a kbucket did not succeed but the kbucket is not full
 pub const KBUCKET_NOT_FULL: &str = "kbucket_not_full";
+
+/// An attempt to send a packet to a peer failed due to a socket mismatch
 pub const SOCK_MISMATCH_ON_SEND: &str = "sock_mismatch_on_send";
+
+/// Failed to return from an event channel
 pub const EV_CHAN_RET_FAIL: &str = "ev_chan_ret_fail";
+
+/// An ENR advertising unreachable IP addresses was encountered (this violates the ENR invariant)
 pub const UNREACHABLE_ENR: &str = "unreachable_enr";
+
+/// A response to a message was received that was not expected
 pub const RECV_RESP_UNEXPECTED: &str = "recv_resp_unexpected";
 
 /// Represents metrics pertaining to errors and warnings that occur throughout
