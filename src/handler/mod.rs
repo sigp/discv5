b34ffdd8a262551be0b06302f0c72d9b7bc95dc3
Age Manning <Age@AgeManning.com>
2025-04-02 13:40:24 +1100

    Sneaky clippy lint

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -786,10 +786,10 @@ impl Handler {
             && match node_address.socket_addr {
                 SocketAddr::V4(socket_addr) => enr
                     .udp4_socket()
-                    .map_or(true, |advertized_addr| socket_addr == advertized_addr),
+                    .is_none_or(|advertized_addr| socket_addr == advertized_addr),
                 SocketAddr::V6(socket_addr) => enr
                     .udp6_socket()
-                    .map_or(true, |advertized_addr| socket_addr == advertized_addr),
+                    .is_none_or(|advertized_addr| socket_addr == advertized_addr),
             }
     }
 
